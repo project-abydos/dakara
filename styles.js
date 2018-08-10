c61f40d9293952b8164d2068290e4e150f8d53c2
Jeff McCoy <jeff@clevrtec.com>
2018-08-10 03:55:28 -0700

    deploy Fri Aug 10 03:55:28 PDT 2018

--- styles.js
+++ styles.js
@@ -7,7 +7,7 @@
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-module.exports = "/* You can add global styles to this file, and also import other style files */\n"
+module.exports = "html, body {\n    width: 100%;\n    height: 100%;\n    margin: 0;\n}"
 
 /***/ }),
 
