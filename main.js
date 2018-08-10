6ab02ae3e00a3b945a92cd76020dacd5d517603a
Racer159 <Racer159>
2018-08-10 16:41:49 -0700

    deploy Fri Aug 10 16:41:48 DST 2018

--- main.js
+++ main.js
@@ -23,6 +23,73 @@ webpackEmptyAsyncContext.id = "./src/$$_lazy_route_resource lazy recursive";
 
 /***/ }),
 
+/***/ "./src/app/action-bar/action-bar.component.css":
+/*!*****************************************************!*\
+  !*** ./src/app/action-bar/action-bar.component.css ***!
+  \*****************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = "button {\r\n    margin-left: 0.5em;\r\n}\r\n\r\n.button-bar {\r\n    margin-top: 0.5em;\r\n    width: 100%;\r\n}"
+
+/***/ }),
+
+/***/ "./src/app/action-bar/action-bar.component.html":
+/*!******************************************************!*\
+  !*** ./src/app/action-bar/action-bar.component.html ***!
+  \******************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = "<nav class=\"navbar navbar-expand-lg navbar-light bg-light\">\r\n  <div class=\"button-bar d-flex justify-content-end\">\r\n    <button class=\"info btn btn-secondary\" title=\"Info\"><fa-icon [icon]=\"['fas', 'info-circle']\"></fa-icon></button>\r\n    <button (click)=\"clearBullets()\" class=\"clear-history btn btn-danger\" title=\"Clear History\"><fa-icon [icon]=\"['fas', 'history']\"></fa-icon></button>\r\n  </div>\r\n</nav>"
+
+/***/ }),
+
+/***/ "./src/app/action-bar/action-bar.component.ts":
+/*!****************************************************!*\
+  !*** ./src/app/action-bar/action-bar.component.ts ***!
+  \****************************************************/
+/*! exports provided: ActionBarComponent */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "ActionBarComponent", function() { return ActionBarComponent; });
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+/* harmony import */ var _bullets_service__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../bullets.service */ "./src/app/bullets.service.ts");
+var __decorate = (undefined && undefined.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (undefined && undefined.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+
+var ActionBarComponent = /** @class */ (function () {
+    function ActionBarComponent(bulletService) {
+        var _this = this;
+        this.bulletService = bulletService;
+        this.clearBullets = function () { return _this.bulletService.clearBullets(); };
+    }
+    ActionBarComponent.prototype.ngOnInit = function () { };
+    ActionBarComponent = __decorate([
+        Object(_angular_core__WEBPACK_IMPORTED_MODULE_0__["Component"])({
+            selector: 'app-action-bar',
+            template: __webpack_require__(/*! ./action-bar.component.html */ "./src/app/action-bar/action-bar.component.html"),
+            styles: [__webpack_require__(/*! ./action-bar.component.css */ "./src/app/action-bar/action-bar.component.css")]
+        }),
+        __metadata("design:paramtypes", [_bullets_service__WEBPACK_IMPORTED_MODULE_1__["BulletsService"]])
+    ], ActionBarComponent);
+    return ActionBarComponent;
+}());
+
+
+
+/***/ }),
+
 /***/ "./src/app/app.component.css":
 /*!***********************************!*\
   !*** ./src/app/app.component.css ***!
@@ -41,7 +108,7 @@ module.exports = ".app-container {\n    height: 100%;\n    display: flex;\n    f
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-module.exports = "<div class=\"app-container container-fluid\">\n  <p>\n    Useful for info for how to use this thing or something...\n    <button (click)=\"clearBullets()\" class=\"btn btn-primary\">Clear History</button>\n  </p>\n  <app-text-list></app-text-list>\n  <app-text-entry></app-text-entry>\n</div>\n"
+module.exports = "<div class=\"app-container\">\n  <nav class=\"navbar navbar-expand-lg navbar-dark bg-dark\">\n    <a class=\"navbar-brand\" href=\"#\">{{ title }}</a>\n    <ul class=\"navbar-nav flex-row ml-auto d-flex\">\n      <li class=\"nav-item\">\n        <a class=\"nav-link\" href=\"{{github_url}}\" title=\"GitHub Project\"><fa-icon [icon]=\"['fab', 'github']\"></fa-icon></a>\n      </li>\n    </ul>\n  </nav>\n  <app-text-list></app-text-list>\n  <app-action-bar></app-action-bar>\n  <app-text-entry></app-text-entry>\n</div>\n"
 
 /***/ }),
 
@@ -56,32 +123,24 @@ module.exports = "<div class=\"app-container container-fluid\">\n  <p>\n    Usef
 __webpack_require__.r(__webpack_exports__);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "AppComponent", function() { return AppComponent; });
 /* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
-/* harmony import */ var _bullets_service__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./bullets.service */ "./src/app/bullets.service.ts");
 var __decorate = (undefined && undefined.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-var __metadata = (undefined && undefined.__metadata) || function (k, v) {
-    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
-};
-
 
 var AppComponent = /** @class */ (function () {
-    function AppComponent(bulletService) {
-        var _this = this;
-        this.bulletService = bulletService;
+    function AppComponent() {
         this.title = 'Dakara';
-        this.clearBullets = function () { return _this.bulletService.clearBullets(); };
+        this.github_url = 'https://github.com/project-abydos/dakara';
     }
     AppComponent = __decorate([
         Object(_angular_core__WEBPACK_IMPORTED_MODULE_0__["Component"])({
             selector: 'app-root',
             template: __webpack_require__(/*! ./app.component.html */ "./src/app/app.component.html"),
             styles: [__webpack_require__(/*! ./app.component.css */ "./src/app/app.component.css")]
-        }),
-        __metadata("design:paramtypes", [_bullets_service__WEBPACK_IMPORTED_MODULE_1__["BulletsService"]])
+        })
     ], AppComponent);
     return AppComponent;
 }());
@@ -104,9 +163,14 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
 /* harmony import */ var _angular_forms__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @angular/forms */ "./node_modules/@angular/forms/fesm5/forms.js");
 /* harmony import */ var _ng_bootstrap_ng_bootstrap__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @ng-bootstrap/ng-bootstrap */ "./node_modules/@ng-bootstrap/ng-bootstrap/index.js");
-/* harmony import */ var _app_component__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./app.component */ "./src/app/app.component.ts");
-/* harmony import */ var _text_entry_text_entry_component__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./text-entry/text-entry.component */ "./src/app/text-entry/text-entry.component.ts");
-/* harmony import */ var _text_list_text_list_component__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ./text-list/text-list.component */ "./src/app/text-list/text-list.component.ts");
+/* harmony import */ var _fortawesome_angular_fontawesome__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! @fortawesome/angular-fontawesome */ "./node_modules/@fortawesome/angular-fontawesome/fesm5/angular-fontawesome.js");
+/* harmony import */ var _app_component__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./app.component */ "./src/app/app.component.ts");
+/* harmony import */ var _text_entry_text_entry_component__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ./text-entry/text-entry.component */ "./src/app/text-entry/text-entry.component.ts");
+/* harmony import */ var _text_list_text_list_component__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(/*! ./text-list/text-list.component */ "./src/app/text-list/text-list.component.ts");
+/* harmony import */ var _action_bar_action_bar_component__WEBPACK_IMPORTED_MODULE_8__ = __webpack_require__(/*! ./action-bar/action-bar.component */ "./src/app/action-bar/action-bar.component.ts");
+/* harmony import */ var _fortawesome_fontawesome_svg_core__WEBPACK_IMPORTED_MODULE_9__ = __webpack_require__(/*! @fortawesome/fontawesome-svg-core */ "./node_modules/@fortawesome/fontawesome-svg-core/index.es.js");
+/* harmony import */ var _fortawesome_free_solid_svg_icons__WEBPACK_IMPORTED_MODULE_10__ = __webpack_require__(/*! @fortawesome/free-solid-svg-icons */ "./node_modules/@fortawesome/free-solid-svg-icons/index.es.js");
+/* harmony import */ var _fortawesome_free_brands_svg_icons__WEBPACK_IMPORTED_MODULE_11__ = __webpack_require__(/*! @fortawesome/free-brands-svg-icons */ "./node_modules/@fortawesome/free-brands-svg-icons/index.es.js");
 var __decorate = (undefined && undefined.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -120,23 +184,31 @@ var __decorate = (undefined && undefined.__decorate) || function (decorators, ta
 
 
 
+
+
+
+
+
+_fortawesome_fontawesome_svg_core__WEBPACK_IMPORTED_MODULE_9__["library"].add(_fortawesome_free_solid_svg_icons__WEBPACK_IMPORTED_MODULE_10__["fas"], _fortawesome_free_brands_svg_icons__WEBPACK_IMPORTED_MODULE_11__["fab"]);
 var AppModule = /** @class */ (function () {
     function AppModule() {
     }
     AppModule = __decorate([
         Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["NgModule"])({
             declarations: [
-                _app_component__WEBPACK_IMPORTED_MODULE_4__["AppComponent"],
-                _text_entry_text_entry_component__WEBPACK_IMPORTED_MODULE_5__["TextEntryComponent"],
-                _text_list_text_list_component__WEBPACK_IMPORTED_MODULE_6__["TextListComponent"]
+                _app_component__WEBPACK_IMPORTED_MODULE_5__["AppComponent"],
+                _text_entry_text_entry_component__WEBPACK_IMPORTED_MODULE_6__["TextEntryComponent"],
+                _text_list_text_list_component__WEBPACK_IMPORTED_MODULE_7__["TextListComponent"],
+                _action_bar_action_bar_component__WEBPACK_IMPORTED_MODULE_8__["ActionBarComponent"]
             ],
             imports: [
                 _angular_platform_browser__WEBPACK_IMPORTED_MODULE_0__["BrowserModule"],
                 _angular_forms__WEBPACK_IMPORTED_MODULE_2__["FormsModule"],
-                _ng_bootstrap_ng_bootstrap__WEBPACK_IMPORTED_MODULE_3__["NgbModule"].forRoot()
+                _ng_bootstrap_ng_bootstrap__WEBPACK_IMPORTED_MODULE_3__["NgbModule"].forRoot(),
+                _fortawesome_angular_fontawesome__WEBPACK_IMPORTED_MODULE_4__["FontAwesomeModule"]
             ],
             providers: [],
-            bootstrap: [_app_component__WEBPACK_IMPORTED_MODULE_4__["AppComponent"]]
+            bootstrap: [_app_component__WEBPACK_IMPORTED_MODULE_5__["AppComponent"]]
         })
     ], AppModule);
     return AppModule;
@@ -211,7 +283,7 @@ var BulletsService = /** @class */ (function () {
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-module.exports = ":host {\n    flex: 0 0 auto;\n    padding: 1em;\n}\n\n.active-bullet-editor {\n    width: 100%;\n    font-family: 'Times New Roman', Times, serif;\n    line-height: 2em;\n}"
+module.exports = ":host {\n    flex: 0 0 auto;\n}\n\n.active-bullet-editor-container {\n    padding: 1em;\n}\n\n.active-bullet-editor {\n    width: 100%;\n    font-family: 'Times New Roman', Times, serif;\n    line-height: 2em;\n}"
 
 /***/ }),
 
@@ -222,7 +294,7 @@ module.exports = ":host {\n    flex: 0 0 auto;\n    padding: 1em;\n}\n\n.active-
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-module.exports = "<input type=\"text\" class=\"form-control active-bullet-editor\" (keyup.enter)=\"update()\" [(ngModel)]=\"activeBullet\" autofocus />"
+module.exports = "<div class=\"active-bullet-editor-container bg-light\">\r\n  <input type=\"text\" class=\"form-control active-bullet-editor\" (keyup.enter)=\"update()\" [(ngModel)]=\"activeBullet\" autofocus />\r\n</div>"
 
 /***/ }),
 
@@ -281,7 +353,7 @@ var TextEntryComponent = /** @class */ (function () {
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-module.exports = ":host {\n    flex: 1 1 auto;\n    overflow: scroll;\n    padding: 1em;\n}\n\n.bullet-entry {\n    font-family: 'Times New Roman', Times, serif;\n    line-height: 2em;\n}\n\n.bullet-entry:nth-child(even) {\n    background: #f7f7f7;\n}"
+module.exports = ":host {\n    flex: 1 1 auto;\n    overflow-y: scroll;\n    padding: 1em;\n}\n\n.bullet-entry {\n    font-family: 'Times New Roman', Times, serif;\n    line-height: 2em;\n}\n\n.bullet-entry:nth-child(even) {\n    background: #f7f7f7;\n}"
 
 /***/ }),
 
