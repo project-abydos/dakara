a82572bfc8a2a555ef5f64b3bd51cfac347ef4fd
Wayne Starr <starr@thanos.local>
2018-08-30 20:13:20 -0700

    feature: added link back to project abydos home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,5 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Dakara';
   github_url = 'https://github.com/project-abydos/dakara';
+  project_abydos_url = 'https://project-abydos.github.io';
 }
